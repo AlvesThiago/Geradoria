c144f631f140e17906467f898b185003c5c5162c
AlvesThiago <thiagodesign.27@gmail.com>
2025-01-01 03:18:25 -0300

    Autenticação e inicio da tela dashboard

---
+++ app/dashboard/_components/SideNav.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { FileClock, Home, Settings, WalletCards } from 'lucide-react'
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react'
+
+function SideNav() {
+
+    const MenuList = [
+        {
+            name: 'Inicio',
+            icon: Home,
+            path:'/dashboard'
+        },
+        {
+            name: 'Histórico',
+            icon: FileClock,
+            path:'/dashboard/historico'
+        },
+        {
+            name: 'Planos',
+            icon: WalletCards,
+            path:'/dashboard/planos'
+        },
+        {
+            name: 'Config',
+            icon: Settings,
+            path:'/dashboard/config'
+        },
+    ]
+
+    const path=usePathname();
+    useEffect(() =>{
+        console.log(path)
+    },[])
+
+  return (
+    <div className='h-screen p-5 shadow-sm border'>
+        <div className='flex justify-center'>
+            <Image src={'/logo.svg'} alt='logo' width={120} height={100} />
+        </div>
+        <hr className='my-8' />
+        <div className='mt-10'>
+            {MenuList.map((menu, index) => (
+                <div className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-cente 
+                ${path==menu.path&&'bg-primary text-white'}
+                `}>
+                    <menu.icon />
+                    <h2>{menu.name}</h2>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default SideNav
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Poppins({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: "300"
 });
 
 export const metadata: Metadata = {
@@ -23,12 +19,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={inter.className}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
