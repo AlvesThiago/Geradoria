2c0bc75b4c3525e3a1be0c0e2be8d536b8240d9e
AlvesThiago <thiagodesign.27@gmail.com>
2025-01-05 22:00:07 -0300

    Corrigindo erro OutputSection

--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,28 +1,35 @@
 'use client'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
-import { Editor } from '@toast-ui/react-editor';
+import { Editor, EditorProps } from '@toast-ui/react-editor';
 import { Button } from '@/components/ui/button';
 import { Copy } from 'lucide-react';
 
-
-interface props{
-  aiOutput:string;
+interface Props {
+  aiOutput: string;
 }
 
-function OutputSection({aiOutput}:props) {
-  const editorRef:any=useRef();
+function OutputSection({ aiOutput }: Props) {
+  // Definindo o tipo corretamente para a referência
+  const editorRef = useRef<Editor>(null);
 
-  useEffect(() =>{
-    const editorInstance=editorRef.current.getInstance();
-    editorInstance.setMarkdown(aiOutput);
-  },[aiOutput])
+  useEffect(() => {
+    if (editorRef.current) {
+      const editorInstance = editorRef.current.getInstance();
+      editorInstance.setMarkdown(aiOutput);
+    }
+  }, [aiOutput]);
 
   return (
-    <div className='bg-white shadow-md border rounded-lg'>
-      <div className='flex justify-between items-center p-2' >
-        <h2 className='font-medium text-lg'>Resultado</h2>
-        <Button className='flex gap-2' onClick={()=>navigator.clipboard.writeText(aiOutput)}><Copy  className='w-4 h-4'/> Copiar</Button>
+    <div className="bg-white shadow-md border rounded-lg">
+      <div className="flex justify-between items-center p-2">
+        <h2 className="font-medium text-lg">Resultado</h2>
+        <Button 
+          className="flex gap-2" 
+          onClick={() => navigator.clipboard.writeText(aiOutput)}
+        >
+          <Copy className="w-4 h-4" /> Copiar
+        </Button>
       </div>
       <Editor
         ref={editorRef}
@@ -30,10 +37,13 @@ function OutputSection({aiOutput}:props) {
         height="600px"
         initialEditType="wysiwyg"
         useCommandShortcut={true}
-        onChange={()=>console.log(editorRef.current.getInstance().getMarkdown())}
+        onChange={() => {
+          const markdown = editorRef.current?.getInstance().getMarkdown();
+          console.log(markdown);
+        }}
       />
     </div>
-  )
+  );
 }
 
-export default OutputSection
+export default OutputSection;
